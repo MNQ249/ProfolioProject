5e9d01b72d31ec1e4f580da74e590ff18bf4bf56
Mesfer <mesfer.vip@gmail.com>
2019-11-09 14:48:12 +0300

    remove list and create variable with array and call this array with another variable and bind this varaiable under ul

--- my-react-app/src/Navigation.js
+++ my-react-app/src/Navigation.js
@@ -4,15 +4,17 @@ import './Navigation.css';
 
 class  Navigation extends Component {
   render(){
+    const sections =['Home','About','Services','Portfolio','Contact'];
+    const navLinks = sections.map( section => { 
+
+      return <li><a href={'#' + section }>{section}</a></li>
+    
+    });
   return (
     <nav>
-      <h2 className="logo">React Project</h2>
+  <h2 className="logo">{this.props.logoTitle}</h2>
       <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">services</a></li>
-        <li><a href="#">Portfolio</a></li>
-        <li><a href="#">Contact</a></li>
+        {navLinks}
       </ul>
     </nav>
   );

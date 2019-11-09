00cad6d3cc8e53ebc6144abc945c11cf5a544bd1
Mesfer <mesfer.vip@gmail.com>
2019-11-09 12:59:04 +0300

    add some style to index.css

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './Navigation.css';
+import './index.css';
 import Navigation from './Navigation';
 import * as serviceWorker from './serviceWorker';
 
